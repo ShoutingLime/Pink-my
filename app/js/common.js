8ea1289511925249e59102285a6725315aebc1f6
ShoutingLime <shouting.lime@gmail.com>
2017-03-27 11:07:34 +0300

    Commit 9
    index.html + graphics + js

--- app/js/common.js
+++ app/js/common.js
@@ -9,7 +9,12 @@ var navMain = document.querySelector('.main-nav'),
   pageHeader = document.querySelector('.page-header'),
   pageHeaderIndex = document.querySelector('.page-header--index'),
   pageHeaderPhoto = document.querySelector('.page-header--photo'),
-  pageHeaderForm = document.querySelector('.page-header--form');
+  pageHeaderForm = document.querySelector('.page-header--form'),
+  reviewsBefore = document.querySelector('.reviews__before'),
+  reviewsAfter = document.querySelector('.reviews__after'),
+  slide1 = document.getElementById('slide1'),
+  slide2 = document.getElementById('slide2'),
+  slide3 = document.getElementById('slide3');
 
 pageHeader.classList.remove('page-header--nojs');
 logo.style.borderBottom = 'none';
@@ -22,24 +27,34 @@ navToggle.addEventListener('click', function (event) {
     navMain.classList.add('main-nav--opened');
     mainNavList.style.display = 'block';
     logo.style.borderBottom = 'solid 1px black';
-    if (window.matchMedia('(max-width: 699px)').matches) {
+    if (window.matchMedia('(max-width: 699px)').matches) { // если mobile
       logo.style.paddingBottom = '25px';
-    } else if (window.matchMedia('(min-width: 700px)').matches && window.matchMedia('(max-width: 1199px)').matches) {
+      if (pageHeaderIndex) {
+        pageHeaderIndex.style.backgroundPosition = '0 435px, -190px 259px';
+        pageHeaderIndex.style.minHeight = '721px';
+      } else if (pageHeaderPhoto) {
+        pageHeaderPhoto.style.backgroundPosition = '-190px 259px';
+        pageHeaderPhoto.style.minHeight = '495px';
+      } else if (pageHeaderForm) {
+        pageHeaderForm.style.backgroundPosition = '-190px 259px';
+        pageHeaderForm.style.minHeight = '495px';
+      }
+    } else if (window.matchMedia('(min-width: 700px)').matches && window.matchMedia('(max-width: 1199px)').matches) { // если tablet
       logo.style.paddingBottom = '43px';
-      download.style.marginTop = '-270px';
-      intro.style.marginTop = '-270px';
-      pageHeaderIndex.style.minHeight = '984px';
+      pageHeaderIndex.style.backgroundPosition = '0 0, -250px 0';
+      pageHeaderIndex.style.height = '984px';
+      if (pageHeaderIndex) {
+        pageHeaderIndex.style.backgroundPosition = '0 440px, -250px 255px';
+        pageHeaderIndex.style.minHeight = '721px';
+      } else if (pageHeaderPhoto) {
+        pageHeaderPhoto.style.backgroundPosition = '-190px 259px';
+        pageHeaderPhoto.style.minHeight = '495px';
+      } else if (pageHeaderForm) {
+        pageHeaderForm.style.backgroundPosition = '-190px 259px';
+        pageHeaderForm.style.minHeight = '495px';
+      }
     }
-    if (pageHeaderIndex) {
-      pageHeaderIndex.style.backgroundPosition = '0 435px, -190px 259px';
-      pageHeaderIndex.style.minHeight = '721px';
-    } else if (pageHeaderPhoto) {
-      pageHeaderPhoto.style.backgroundPosition = '-190px 259px';
-      pageHeaderPhoto.style.minHeight = '495px';
-    } else if (pageHeaderForm) {
-      pageHeaderForm.style.backgroundPosition = '-190px 259px';
-      pageHeaderForm.style.minHeight = '495px';
-    }
+    // здесь будет код для desktop
   } else { // если кликаем по крестику
     navMain.classList.remove('main-nav--opened');
     navMain.classList.add('main-nav--closed');
@@ -49,9 +64,8 @@ navToggle.addEventListener('click', function (event) {
       logo.style.paddingBottom = '25px';
     } else if (window.matchMedia('(min-width: 700px)').matches && window.matchMedia('(max-width: 1199px)').matches) {
       logo.style.paddingBottom = '43px';
-      download.style.marginTop = '-270px';
-      intro.style.marginTop = '-270px';
-      pageHeaderIndex.style.minHeight = '984px';
+      pageHeaderIndex.style.backgroundPosition = '0 176px, -250px 0';
+      pageHeaderIndex.style.height = '721px';
     }
     if (pageHeaderIndex) {
       pageHeaderIndex.style.backgroundPosition = '0 176px, -190px 0';
@@ -63,5 +77,41 @@ navToggle.addEventListener('click', function (event) {
       pageHeaderForm.style.backgroundPosition = '-190px 0';
       pageHeaderForm.style.minHeight = '237px';
     }
+  }
+});
+
+reviewsBefore.addEventListener('click', function (event) {
+  'use strict';
+  event.preventDefault();
+  if (window.getComputedStyle(slide1).display === 'block') {
+    slide1.style.display = 'none';
+    slide2.style.display = 'none';
+    slide3.style.display = 'block';
+  } else if (window.getComputedStyle(slide2).display === 'block') {
+    slide2.style.display = 'none';
+    slide1.style.display = 'block';
+    slide3.style.display = 'none';
+  } else if (window.getComputedStyle(slide3).display === 'block') {
+    slide3.style.display = 'none';
+    slide2.style.display = 'block';
+    slide1.style.display = 'none';
+  }
+});
+
+reviewsAfter.addEventListener('click', function (event) {
+  'use strict';
+  event.preventDefault();
+  if (window.getComputedStyle(slide1).display === 'block') {
+    slide1.style.display = 'none';
+    slide2.style.display = 'block';
+    slide3.style.display = 'none';
+  } else if (window.getComputedStyle(slide2).display === 'block') {
+    slide2.style.display = 'none';
+    slide1.style.display = 'none';
+    slide3.style.display = 'block';
+  } else if (window.getComputedStyle(slide3).display === 'block') {
+    slide3.style.display = 'none';
+    slide2.style.display = 'none';
+    slide1.style.display = 'block';
   }
 });
